0f9d1dc6ec56f08d0ba3326a57b5b964960818d4
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-01 22:16:39 +0100

    public key cryptography

--- Blockchain_Cryptography/Digital_Signatures/signMessage.js
+++ Blockchain_Cryptography/Digital_Signatures/signMessage.js
@@ -34,5 +34,6 @@ const PRIVATE_KEY =
   "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
 
 async function signMessage(msg) {
-  const hashedmessage = await hashMessage(msg);
+  const messageHash = await hashMessage(msg);
+  return secp.sign(messageHash, PRIVATE_KEY, { recovered: true });
 }
