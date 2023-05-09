0fe3e71877ccf94fdfd92735d21e2fce53224699
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-09 11:49:26 +0100

    Mempool

---
+++ Blockchain_Cryptography/Proof_of_Work/index.js
@@ -0,0 +1,14 @@
+/**
+ * The Mempool
+Users who want to make transactions will broadcast their transactions to the blockchain network. The mempool is a place for miners to keep those transactions before adding them to a block.
+
+Typically, the miner will take all the transactions with the highest transaction fees from the mempool. Then they'll add them to the block and attempt to find the proof of work.
+
+ You can find more information on bitcoin miner fee priority here: https://en.bitcoin.it/wiki/Miner_fees#Priority_transactions
+
+ Your Goal: Add Transaction
+Your goal is to implement the addTransaction function, which adds transactions to the mempool.
+
+Take the transaction sent to the function and push it on top of the mempool array.
+
+ */
