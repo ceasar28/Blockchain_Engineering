e425076e1ed5d6ec7b25866ca7af1e6cc1695c41
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-09 11:57:20 +0100

    Mempool

--- Blockchain_Cryptography/Proof_of_Work/index.js
+++ Blockchain_Cryptography/Proof_of_Work/index.js
@@ -12,3 +12,21 @@ Your goal is to implement the addTransaction function, which adds transactions t
 Take the transaction sent to the function and push it on top of the mempool array.
 
  */
+
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+  BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+  // TODO: add transaction to mempool
+  mempool.unshift(transaction);
+  return;
+}
+
+function mine() {
+  // TODO: mine a block
+}
