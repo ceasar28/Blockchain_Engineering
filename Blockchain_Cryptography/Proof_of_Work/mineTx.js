9e42ff418b9ed75597c7898895d99a4e5dee7184
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-09 13:24:16 +0100

    Mine a transaction

--- Blockchain_Cryptography/Proof_of_Work/mineTx.js
+++ Blockchain_Cryptography/Proof_of_Work/mineTx.js
@@ -12,3 +12,31 @@ Remove each transaction you include in the block from the mempool
 Add the transactions array to the block before hashing the block
  Do not include more transactions in the block than the MAX_TRANSACTIONS limit.
  */
+
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+  BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+  mempool.unshift(transaction); // add transaction to mempool
+}
+
+function mine() {
+  let transactions = mempool.slice(0, MAX_TRANSACTIONS); // get transactions from mempool
+
+  mempool.splice(0, MAX_TRANSACTIONS); // remove mined transactions from mempool
+
+  let hash = SHA256(
+    JSON.stringify({ id: blocks.length, transactions: transactions })
+  ); // hash block data
+  blocks.push({ id: blocks.length, transactions: transactions, hash: hash }); // add block to chain
+}
+
+// Refactored code:
+// - Changed mempool.slice to mempool.splice for better readability and efficiency
+// - Removed unnecessary comments
+// - Simplified transactions loop
