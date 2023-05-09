56634fe826f3c701e0c7c67563c0408b420b146e
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-09 12:24:56 +0100

    Mine a block

--- Blockchain_Cryptography/Proof_of_Work/mineBlock.js
+++ Blockchain_Cryptography/Proof_of_Work/mineBlock.js
@@ -11,3 +11,22 @@ Update the mine() function to create a new block object with a single property: 
 Set the id property to the block height prior to the new block being added
 Push the block object into the blocks array
  */
+
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+  BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+  // TODO: add transaction to mempool
+  mempool.unshift(transaction);
+  return;
+}
+
+function mine() {
+  // TODO: mine a block
+  blocks.push({ id: blocks.length });
+}
