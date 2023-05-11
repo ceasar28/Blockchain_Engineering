69324a229e31e8690bd05944000530d70ed47e4e
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-11 08:27:13 +0100

    block hash

--- Blockchain_Cryptography/Blockchain_Network/Blockchain_Data_Structure/Block.js
+++ Blockchain_Cryptography/Blockchain_Network/Blockchain_Data_Structure/Block.js
@@ -51,6 +51,11 @@ In your Block.js file, we have a class Block. Using the SHA256 function from the
 For now, there's no need to hash anything in particular since the block contains none of the components we mentioned above.
 
  Feel free to hash a message, your own name, or nothing at all! So long as it is a 64 character hexadecimal string you will pass this stage.
-
-
  */
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  toHash() {
+    return SHA256("Emmanuel");
+  }
+}
