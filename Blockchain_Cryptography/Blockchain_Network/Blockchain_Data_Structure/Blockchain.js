86a9df70124b29c57ddfd31e6d10204f9475d4ab
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-11 08:41:56 +0100

    creating the Genesis Block

--- Blockchain_Cryptography/Blockchain_Network/Blockchain_Data_Structure/Blockchain.js
+++ Blockchain_Cryptography/Blockchain_Network/Blockchain_Data_Structure/Blockchain.js
@@ -4,6 +4,17 @@ We have a new file: Blockchain.js. How exciting!
 
 This stage is going to focus on adding the first block to our new Blockchain class! The first block is often referred to as the Genesis Block.
 
+Genesis Block
+The genesis block is the first block in the chain, where it all kicks off! Every block after the genesis block links back to the first one, but the genesis block has no previous block! This is important to keep in mind for the next few stages. 
+
+Here are some examples of genesis blocks on live blockchains displayed in their respective block explorers:
+
+Bitcoin Genesis Block
+Here's Bitcoin's Genesis Block on Block Explorer on January 3rd, 2009. https://blockstream.info/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f
+
+Ethereum Genesis Block
+Here's Ethereum's Genesis Block on EtherScan on July 30th, 2015. https://etherscan.io/block/0
+
  Your Goal: Add the Genesis Block
 The Blockchain.js file contains the Blockchain class with a chain array. Let's add the Genesis Block to this array.
 
