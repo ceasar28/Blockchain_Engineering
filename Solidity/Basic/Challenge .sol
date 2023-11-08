05239a0ed736f486953e48f3318c175957fe03a3
ceasar28 <emmanuelugochukwuekete@gmail.com>
2023-11-08 22:53:54 +0100

    datatypes

--- Solidity/Basic/Challenge .sol
+++ Solidity/Basic/Challenge .sol
@@ -22,5 +22,7 @@ return "Successfuly set";
  }
 }
 
+// https://academy.metacrafters.io/swap
+
 // Challenge
 // Write a simple contract and declare four different types of variables. Write get and set functions for each of these variables. Return the value of the variable in the “set function”.
---
+++ Solidity/Basic/Datatypes.md
@@ -0,0 +1,5 @@
+A data type is an attribute of data that tells the compiler how the programmer intends to use the data.
+
+Data types in Solidity can be classified into two:
+1 values : contains a values(unsigned integer, signed integers, boolean, strings)
+2 References( array, map, struct)
