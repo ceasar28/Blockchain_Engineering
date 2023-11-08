623fa1fdb1faf9e305b99496bf10cf125def81f6
ceasar28 <emmanuelugochukwuekete@gmail.com>
2023-11-09 00:18:21 +0100

    datatypes

---
+++ Solidity/Basic/Challenge2.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Challenge2 {
+
+
+
+
+//     Primitive Data Types in Solidity
+// Challenge
+// Write a smart contract that takes some ether from the user. Find out its value in units of:
+
+// wei
+// ether
+// gwei
+// Share your contract in our Discord channel.
+
+// We know it is a bit vague and flexible. That's on purpose! Be creative and see what you can build.
+}
